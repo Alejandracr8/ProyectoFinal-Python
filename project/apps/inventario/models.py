e8751129c44efc765054725880d276f8cefcf2f0
Alejandra Caicedo <alejacr34@gmail.com>
2023-07-29 10:28:05 -0400

    Modelos ingresados

--- project/apps/inventario/models.py
+++ project/apps/inventario/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Ubicacion(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+   
+
+class Cilindro(models.Model):
+    numero_serie = models.CharField(max_length=100, unique=True)
+    tipo_gas = models.CharField(max_length=50)
+    capacidad = models.IntegerField()
+    ubicacion = models.ForeignKey(Ubicacion, on_delete=models.CASCADE)  
